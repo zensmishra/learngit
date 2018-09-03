5cb48dd2188431786b4ecfb7a289343ffb37a069
Sanket Mishra <smishra@zendesk.com>
2018-09-03 10:56:29 -0700

    Modified def statements

--- testfile.py
+++ testfile.py
@@ -1,9 +1,9 @@
-    def getPrint():
-        print("World is great for the rich")
+    def getPrint( person ):
+        print("World is great for the rich" + person)
 
     # Added comments    
     def main():
-        getPrint()
+        getPrint("Rahul")
 
 if __name__ == "__main__":
     # This is a main
